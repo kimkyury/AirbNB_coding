354cbde77773ba59e3894a75f351fa7cdbb90cfd
kimkyury <mandarining0918@gmail.com>
2021-08-28 17:33:32 +0900

    Feat: 필요한 패키지 설치

---
+++ test_server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports={//local 의 환경에서는 다음과 같이 사용하고, 배포 이후에는 HEROKU 에서 별도로 URI 를 지정해줘서 사용할 수 있도록 한다
+    mongoURI:'mongodb+srv://ardish_test:dblabtest@cluster0.we71b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
---
+++ test_server/index.js
@@ -0,0 +1,50 @@
+require("dotenv").config(); // .env 파일 적용
+const express = require('express');
+const cors = require('cors'); // B-F 서버 차이(CORS)해결을 위한 미들웨어
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const app =express();
+const path = require("path");
+const config = require("./config/key");
+const mongoose = require("mongoose");
+const port = process.env.PORT
+
+const connect = mongoose.connect(config.mongoURI,{
+        userNewUrlParser: true, 
+        useUnifiedTopology: true,
+        userCreateIndex: true, 
+        useFindAndModify: false
+    }).then(() => console.log('✔️ MongoDB 연결했어요 ')).catch(err => console.log(err));
+
+
+/* 패키지 적용 */
+// 서버 차이 해결
+app.use(cors()) 
+// 가공된 형태로 보내고-받고-접근할 수 있도록 bodyParser 적용 => 디폴트값(Undefind)오류 해결
+app.use(bodyParser.urlencoded({ extended: true})); 
+app.use(bodyParser.json());
+//요청된 쿠키를 쉽게 추출하도록 함
+app.use(cookieParser()); 
+
+
+/* api 생성 */
+app.use('/api/menus', require('./api/menus'));
+app.get('/api/menus', (req, res) => {
+    console.log('  ', menu);
+    res.json(" 🚩 api/menus가 호출되었습니다. ");
+});
+app.post('api/menu', (req, res) => {
+    const menu = req.body.menu;
+    console.log('➕ menu 추가하기 ➕', menu);
+    menus.push(menu);
+    res.json("menu를 추가했습니다. ");
+});
+app.get('/', (req, res) => {
+    res.send(" 🚩 App이 작동중입니다. ");
+});
+
+
+app.listen(port, () => {
+    console.log(`✔️ ${port}에서 대기하고 있습니다.`)
+});
