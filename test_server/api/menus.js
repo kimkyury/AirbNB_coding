1b9da42911df4745a1aa9bf3deafed0294ed323b
kimkyury <mandarining0918@gmail.com>
2021-08-30 00:17:54 +0900

    Feat: API제작

--- test_server/api/menus.js
+++ test_server/api/menus.js
@@ -1,29 +1,28 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var Menu = require('../models/menu');
-var mongoose = require('mongoose');
-const { query } = require('express');
-
+var Menu = require("../models/menu");
+var mongoose = require("mongoose");
+const { query } = require("express");
 
 /* 정렬하기: 특정 어절에 대하여, 메뉴이름을 정렬하여 보여준다 */
-router.get('/',
-    function(req, res, next) {
-        var query = {};
-        //menu이름의 일부어절만을 검색한 상태라면
-        if(req.query.menuName) query.menuName = {$regex:req.query.menuName, $options:'i'};
-        
-        Menu.find(query)        // 해당 어절 searching
-        .sort({menuName: 1})    // menuName을 오름차순sorting
-        .exec(function(err, menus) {     // 오류 안내
-            if(err) {
-                res.status(500);
-                res.json({success:false, message:err});
-            }
-            else {  // 성공시 정렬된 data전송
-                res.json({success:true, data:menus});
-            }
-        });
-    }
-);
+router.get("/", function (req, res, next) {
+  var query = {};
+  //menu이름의 일부 어절을 검색한 상태라면
+  if (req.query.menuName)
+    query.menuName = { $regex: req.query.menuName, $options: "i" };
+
+  Menu.find(query) // 해당 어절 searching
+    .sort({ menuName: 1 }) // menuName을 오름차순sorting
+    .exec(function (err, menus) {
+      // 오류 안내
+      if (err) {
+        res.status(500);
+        res.json({ success: false, message: err });
+      } else {
+        // 성공시 정렬된 data전송
+        res.json({ success: true, data: menus });
+      }
+    });
+});
 
 module.exports = router;
--- test_server/server.js
+++ test_server/server.js
@@ -1,53 +1,51 @@
 require("dotenv").config(); // .env 파일 적용
-const express       = require('express');
-const cors          = require('cors'); // B-F 서버 차이(CORS)해결을 위한 미들웨어
-const bodyParser    = require("body-parser");
-const cookieParser  = require("cookie-parser");
+const express = require("express");
+const cors = require("cors"); // B-F 서버 차이(CORS)해결을 위한 미들웨어
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const app       = express();
-const path      = require("path");
-const config    = require("./config/key");
-const mongoose  = require("mongoose");
-const port      = process.env.PORT
+const app = express();
+const path = require("path");
+const config = require("./config/key");
+const mongoose = require("mongoose");
+const port = process.env.PORT;
 
 mongoose.Promise = global.Promise;
-const connect = mongoose.connect(config.mongoURI)
-    .then(() => console.log('✔️ MongoDB 연결했어요 '))
-    .catch(err => console.log(err));
-
+const connect = mongoose
+  .connect(config.mongoURI)
+  .then(() => console.log("✔️ MongoDB 연결했어요 "))
+  .catch((err) => console.log(err));
 
 /* 패키지 적용 */
 // 서버 차이 해결
-app.use(cors()) 
+app.use(cors());
 // 가공된 형태로 보내고-받고-접근할 수 있도록 bodyParser 적용 => 디폴트값(Undefind)오류 해결
-app.use(bodyParser.urlencoded({ extended: true})); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 //요청된 쿠키를 쉽게 추출하도록 함
-app.use(cookieParser()); 
-
+app.use(cookieParser());
 
 /* api 생성 */
-app.use('/api', (req, res) => res.json({menuName:'베이컨아보카도샌드위치'}));
-app.use('/api/menus', require('./api/menus'));  // api폴더 > menus.js 내용 참고
+app.use("/api/restaurantName", require("./api/menus")); // api폴더 > menus.js 내용 참고
+app.use("/api/menu", require("./api/menus")); // api폴더 > menus.js 내용 참고
 
-
-
-app.get('/api/menus', (req, res) => {
-    console.log('  ', menu);
-    res.json(" 🚩 api/menus가 호출되었습니다. ");
+app.get("/api/menus", (req, res) => {
+  console.log("  ", menu);
+  res.json(" 🚩 api/menus가 호출되었습니다. ");
 });
-app.get('/', (req, res) => {
-    res.send(" 🚩 App이 작동 중입니다. ");
+app.get("/", (req, res) => {
+  res.send(" 🚩 App이 작동 중입니다. ");
 });
-app.post('api/menu', (req, res) => {
-    const menu = req.body.menu;
-    console.log('➕ menu 추가하기 ➕', menu);
-    menus.push(menu);
-    res.json("menu를 추가했습니다. ");
+app.post("api/menu", (req, res) => {
+  const menu = req.body.menu;
+  console.log("➕ menu 추가하기 ➕", menu);
+  menus.push(menu);
+  res.json("menu를 추가했습니다. ");
 });
-
 
 /* 서버 연결 */
 app.listen(port, () => {
-    console.log(`✔️ http://localhost:`+port + `, ${port}port에서 대기하고 있습니다.`)
+  console.log(
+    `✔️ http://localhost:` + port + `, ${port}port에서 대기하고 있습니다.`
+  );
 });
