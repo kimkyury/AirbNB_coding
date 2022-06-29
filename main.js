8bab59c8d7f6392293a5c7b84c363cd475e0430d
kgyury <mandarining0918@gmail.com>
2022-06-30 01:19:10 +0900

    init setting

--- main.js
+++
@@ -1,7 +0,0 @@
-.then(res => res.json())    // 서버로부터 받음
-.then(json => {
-    console.log(json);      
-    console.log(json.list[0].man-file);     //dataTime (측정시간) 콘솔 출력
-    //console.log(json.list[0].);    //pm10Value 미세먼지 지수 콘솔출력
-    //console.log(json.list[0].);    //pm25Value 미세먼지 지수 콘솔출력
-});
