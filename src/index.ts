a5cd74400983378e400a7315871c87e3d3f428fa
kgyury <mandarining0918@gmail.com>
2022-06-30 14:16:26 +0900

    feat: implement blockchain

--- src/index.ts
+++ src/index.ts
@@ -26,16 +26,38 @@ class Block implements BlockShape {
     }
 } 
 
-
 class Blockchain {
-    private blocks: Block[]
+    private blocks: Block[];
     constructor(){
-        this.blocks = []
+        this.blocks = [];
     }
 
+    // Block의 이전의 마지막 hash값을 리턴
+    private getPrevHash() {
+        if(this.blocks.length === 0) return "";
+        return this.blocks[this.blocks.length-1].hash;
+    }
     
-
+    // Block에 chain 추가
     public addBlock(data:string){
-
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length +1,data);
+        this.blocks.push(newBlock);
+    }
+    
+    // block 정보 접근
+    public getBlocks() {
+        // 보안을 위하여 아예 새로운 배열을 리턴하는 방식으로 접근 허용
+        return [...this.blocks];
     }
 }
+
+
+const blockchain = new Blockchain();
+blockchain.addBlock("fisrt");
+blockchain.addBlock("second");
+blockchain.addBlock("third");
+
+// 해킹시도: 해킹이 되었다면 새로운 블록이 추가된 상태로 log가 찍혀야한다
+blockchain.getBlocks().push(new Block("xxxx",  111, "TRY-HACKED"))
+// getBlock 함수에서 아예 새로운 배열을 리턴받았으므로, 원본의 변경은 일어나지 않았다
+console.log(blockchain.getBlocks());
